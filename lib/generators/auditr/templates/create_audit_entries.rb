28ab395cbbf9091e5b8d3c869aa23fe19d33457c
Gareth Bradley <gb@garethbradley.co.uk>
2013-03-22 15:26:18 +0000

    Fixed an issue in the migration template

--- lib/generators/auditr/templates/create_audit_entries.rb
+++ lib/generators/auditr/templates/create_audit_entries.rb
@@ -4,11 +4,11 @@ class CreateAuditEntries < ActiveRecord::Migration
       t.string   :item_type, :null => false
       t.integer  :item_id,   :null => false
       t.string   :event,     :null => false
-      t.string   :entry,
-      t.string   :user,
-      t.string   :severity,
-      t.text     :object,
-      t.text     :changes,
+      t.string   :entry
+      t.string   :user
+      t.string   :severity
+      t.text     :object
+      t.text     :changes
       t.datetime :created_at
     end
     add_index :audit_entries, [:item_type, :item_id]
