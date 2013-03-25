a5d9b4d3df22093bf93e09fa1387fd9eddd24a21
Gareth Bradley <gb@garethbradley.co.uk>
2013-03-25 11:35:41 +0000

    Fixed an issue with the auditr AuditEntry model being missing

---
+++ lib/auditr/audit_entry.rb
@@ -0,0 +1,22 @@
+class AuditEntry < ActiveRecord::Base
+  belongs_to :item, :polymorphic => true
+
+  validates_presence_of :event, :severity
+  attr_accessible :item_type, :item_id, :event, :entry, :user, :object, :changes
+
+  def self.with_item_keys(item_type, item_id)
+    where :item_type => item_type, :item_id => item_id
+  end
+
+  def self.creates
+    where :event => 'create'
+  end
+
+  def self.updates
+    where :event => 'update'
+  end
+
+  def self.destroys
+    where :event => 'destroy'
+  end
+end
