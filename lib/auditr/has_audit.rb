aa62331dcce85806665dca879ac3e67c2e3801eb
Gareth Bradley <gb@garethbradley.co.uk>
2013-03-27 11:36:10 +0000

    Added reflection function to query child audit entries

--- lib/auditr/has_audit.rb
+++ lib/auditr/has_audit.rb
@@ -85,19 +85,20 @@ module Auditr
         associations = self.reflections.select{|s,r| [:has_many, :has_one].include? r.macro}
 #        child_audit_entries = self.audit_entries
         t = AuditEntry.arel_table
-        query = t[:item_type].matches(self.class).and(t[:item_id].matches(self.id))
+        query = t[:item_type].matches(self.class.name).and(t[:item_id].matches(self.id))
 
         associations.each do |association|
           begin
         #    child_audit_entries << send(association.last.plural_name).audit_entries
             send(association.last.plural_name).each do |child_record|
 #              child_audit_entries = (child_audit_entries or child_record.audit_entries) unless child_record.audit_entries.blank?         
-              query.or(t[:item_type].matches(child_record.class).and(t[:item_id].matches(child_record.id)))
+              query.or(t[:item_type].matches(child_record.class.name).and(t[:item_id].matches(child_record.id)))
             end
 
           rescue
           end
         end
+        raise query.to_sql
 
         return AuditEntry.where(query)
 
